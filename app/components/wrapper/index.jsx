fb9f8bc9fcb5c37fd8104a94c6a867cc86942cb8
Taras Chepura <taras.chepura@geeksforless.com>
2022-04-28 21:02:10 +0300

    Fixed overflow

--- app/components/wrapper/index.jsx
+++ app/components/wrapper/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../header/index.jsx';
 
 function Wrapper (props) {
-  return <div className="container">
+  return <div className="container p-0">
     <Header />
     {props.children}
   </div>
